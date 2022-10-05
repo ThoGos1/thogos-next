dcfb6cc427c67bcf5d45b0d2fd3218b6bdcecc27
ThoGos1 <tgosport@gmail.com>
2022-10-05 16:01:05 +0200

    NOW config done

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['images.ctfassets.net'] // This makes images shown possible, AKA a whitelist
+  },
 }
 
 module.exports = nextConfig
