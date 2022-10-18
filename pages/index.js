bb27ca6fa288c8fe2ce0f5d7779bf55882ad6f49
ThoGos1 <tgosport@gmail.com>
2022-10-18 20:50:26 +0200

    "fixed" the home page angular

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 export default function Home() {
   return (
-    <div>
+    <div className="grid">
       <title>Home</title>
       
+      <div></div>
 
+      <div>
+        <span className="logo-1 drop">Welcome to</span>
+        <span className="logo-2">ThoGos</span>
+        <span className="logo-3">.com</span>
+      </div>
 
-
-
-      <p className="drop">down animation</p>
-      <p>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/>2<br/></p>
+      <div></div>
+    
     </div>  
   )
 }
