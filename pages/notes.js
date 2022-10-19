9b420fc1c8a7b3bf92f4942b215cd4d1d8e4e5b8
ThoGos1 <tgosport@gmail.com>
2022-10-20 01:50:08 +0200

    card ready for development

--- pages/notes.js
+++ pages/notes.js
@@ -1,4 +1,7 @@
 import Head from "next/head"
+import Image from "next/image"
+import logo from "../public/logo.png"
+import Link from "next/link"
 
 export default function Notes() {
     return (
@@ -12,9 +15,15 @@ export default function Notes() {
 
         
         <div className="noteshow">
-          <div className="card">
-            Sewey 1
-          </div>
+
+          <Link href={'notes/test-note'}>
+            <div className="card">
+              <div className="cardimg">
+                <Image src={logo} height={64} width={64} alt="ThoGos-logo"/>
+              </div>
+              Chemistry
+            </div>
+          </Link>
 
           <div className="card">
             Sewey 2
