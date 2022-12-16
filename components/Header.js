4135b77beb53a6b291d448604dd50a76d4705bfb
ThoGos1 <tgosport@gmail.com>
2022-12-16 21:15:09 +0100

    svg

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,14 @@ export default function Header() {
       <div style={{ cursor: "pointer", width: "64px", height: "64px", marginLeft: "15px", marginTop: "15px" }}>
         <Link href={'/'}>
           <div>
-            <Image src={tlogo} height={42} width={42} alt="ThoGos-logo"/>
+          <svg width="42" height="42" viewBox="0 0 180 180" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="60" y="180" width="180" height="60" rx="10" transform="rotate(-90 60 180)" fill="#34A855"/>
+        <rect width="180" height="60" rx="10" fill="#EA4333"/>
+        <rect width="60" height="60" rx="10" fill="#4285F5"/>
+        <rect x="31" width="29" height="60" fill="#4285F5"/>
+        <rect x="60" y="60" width="60" height="49" rx="10" fill="#FBBC00"/>
+        <rect x="60" y="60" width="60" height="25" fill="#FBBC00"/>
+      </svg>
           </div>
         </Link>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Image from "next/image"
 import tlogo from "../public/t-logo.png"
+import big from "../public/big.png"
 
 export default function Home() {
   
@@ -22,7 +23,15 @@ export default function Home() {
         <span className="logo-3">.com</span>
       </div>
 
-      <Image src={tlogo} height={85} width={85} alt="ThoGos-logo"/>
+
+      <svg width="85" height="85" viewBox="0 0 180 180" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="60" y="180" width="180" height="60" rx="10" transform="rotate(-90 60 180)" fill="#34A855"/>
+        <rect width="180" height="60" rx="10" fill="#EA4333"/>
+        <rect width="60" height="60" rx="10" fill="#4285F5"/>
+        <rect x="31" width="29" height="60" fill="#4285F5"/>
+        <rect x="60" y="60" width="60" height="49" rx="10" fill="#FBBC00"/>
+        <rect x="60" y="60" width="60" height="25" fill="#FBBC00"/>
+      </svg>
 
     </div>  
   )
