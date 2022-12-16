26bc83cd220eae9bb73a4e1f82d030e1e8febb7c
ThoGos1 <tgosport@gmail.com>
2022-12-16 22:16:36 +0100

    cornicon

--- pages/index.js
+++ pages/index.js
@@ -24,14 +24,13 @@ export default function Home() {
       </div>
 
 
-      <svg width="85" height="85" viewBox="0 0 180 180" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect x="60" y="180" width="180" height="60" rx="10" transform="rotate(-90 60 180)" fill="#34A855"/>
-        <rect width="180" height="60" rx="10" fill="#EA4333"/>
-        <rect width="60" height="60" rx="10" fill="#4285F5"/>
-        <rect x="31" width="29" height="60" fill="#4285F5"/>
-        <rect x="60" y="60" width="60" height="49" rx="10" fill="#FBBC00"/>
-        <rect x="60" y="60" width="60" height="25" fill="#FBBC00"/>
-      </svg>
+      <svg width="85" height="95" viewBox="0 0 180 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M60 200L60 67H120V180C120 191.046 111.046 200 100 200H60Z" fill="#34A855"/>
+<path d="M0 20C0 8.9543 8.95431 0 20 0L60 0V67H0L0 20Z" fill="#4285F5"/>
+<path d="M60 91L90 61L120 91L90 121L60 91Z" fill="#FBBC00"/>
+<path d="M60 0L180 0V47C180 58.0457 171.046 67 160 67H60V0Z" fill="#EA4333"/>
+<path d="M60 67H120V91H60V67Z" fill="#FBBC00"/>
+</svg>
 
     </div>  
   )
