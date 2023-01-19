6926376e573a873816fb1b454af0fe354e65fde0
ThoGos1 <tgosport@gmail.com>
2023-01-19 18:29:36 +0100

    compact

--- components/Layout.js
+++ components/Layout.js
@@ -12,14 +12,9 @@ export default function Layout({ children }) {
     <div>
 
       <Header>
-
         <NavItem icon="Notes">
-
           <DropdownMenu />
-          
         </NavItem>
-
-
       </Header>
 
 
